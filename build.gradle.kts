0f1b8fa7e167c698065157927f497343a5175ca6
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-10-26 10:50:58 +0100

    Refactor root build.gradle to Kotlin.
    - Everything but the task.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.2.71'
+    val kotlinVersion: String by extra { "1.2.71" }
     repositories {
         google()
         jcenter()
-        maven {
-            url "https://plugins.gradle.org/m2/"
-        }
+        maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.2.1")
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -19,7 +17,7 @@ buildscript {
 }
 
 plugins {
-    id "com.dorongold.task-tree" version "1.3.1"
+    id("com.dorongold.task-tree") version("1.3.1")
 }
 
 allprojects {
@@ -29,6 +27,8 @@ allprojects {
     }
 }
 
+/*
 task clean(type: Delete) {
     delete rootProject.buildDir
 }
+*/
